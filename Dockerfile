82855eeb11d164dd8cb234315d522b59c87a65f2
Muhammad Nadzhif Fikri <nadzhiff@gmail.com>
2025-12-05 12:57:08 +0700

    app port env rename

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,15 @@ FROM golang:1.25.4-trixie
 
 WORKDIR /app
 
-COPY . .
+COPY go.mod go.sum ./
 
 RUN go mod download
 
+COPY . .
+
 RUN go build main.go
 
-ENV AppPort=3000
+ENV APP_PORT=3000
 
 EXPOSE 3000
 
--- app/config/config.go
+++ app/config/config.go
@@ -7,12 +7,9 @@ import (
 )
 
 type Config struct {
-	Neo4jUri      string
-	Neo4jUser     string
-	Neo4jPassword string
-	GorseURL      string
-	GorseKey      string
-	AppPort       string
+	GorseURL string
+	GorseKey string
+	AppPort  string
 }
 
 func Load() (*Config, error) {
@@ -21,11 +18,8 @@ func Load() (*Config, error) {
 		return nil, err
 	}
 	return &Config{
-		Neo4jUri:      os.Getenv("NEO4J_URI"),
-		Neo4jUser:     os.Getenv("NEO4J_USER"),
-		Neo4jPassword: os.Getenv("NEO4J_PASSWORD"),
-		GorseURL:      os.Getenv("GORSE_URL"),
-		GorseKey:      os.Getenv("GORSE_KEY"),
-		AppPort:       os.Getenv("AppPort"),
+		GorseURL: os.Getenv("GORSE_URL"),
+		GorseKey: os.Getenv("GORSE_KEY"),
+		AppPort:  os.Getenv("APP_PORT"),
 	}, nil
 }
